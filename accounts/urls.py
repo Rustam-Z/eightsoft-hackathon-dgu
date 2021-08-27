960ab98188fcbb43898222e4c377379b12c91971
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-08-27 16:58:33 +0500

    profile update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import SignupPageView
+from .views import (
+     SignupPageView,
+     AccountUpdateView,
+)
 
 urlpatterns = [
      path('signup/', SignupPageView.as_view(), name='signup'),
+     
+     path('<slug:slug>/update',
+          AccountUpdateView.as_view(),
+          name='account_update'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import CustomUserCreationForm
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.edit import UpdateView, DeleteView, CreateView
+from .models import CustomUser
 
 # Create your views here.
 
@@ -11,3 +13,29 @@ class SignupPageView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('account_login')
     template_name = 'account/signup.html'
+
+
+class AccountUpdateView(LoginRequiredMixin,
+                        UserPassesTestMixin,
+                        UpdateView):
+    model = CustomUser
+    template_name = 'profile/profile_update.html'
+    login_url = 'account_login'
+    fields = (
+        'email',
+        'phone',
+        'image',
+        'telegram',
+        'region',
+    )
+
+    def test_func(self):
+        obj = self.get_object()
+        return obj.slug == self.request.user.slug
+    
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+    # def get_success_url(self):
+    #     return reverse_lazy('account_detail',
+    #                         kwargs={'slug': self.request.user.slug})
