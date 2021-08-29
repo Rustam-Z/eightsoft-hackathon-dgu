971d7dd0cdf3c4a32de1863b635ee7a3e05cba51
Alimov-8 <alimov.abdullokh8@gmail.com>
2021-08-29 12:47:34 +0500

    Match logic improved

--- books/views.py
+++ books/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.mixins import (
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.db.models import Q
+import operator
+import functools
 from django.urls import reverse_lazy, reverse
 from django.shortcuts import redirect
 
@@ -83,39 +85,44 @@ def match_results(request):
     if request.user.is_anonymous:
         return redirect('account_login')
     
+    
     context = dict()
     
+    # Books I need and others have
     user_need_books = BookNeed.objects.filter(user=request.user)
     user_need_books_name = list()
     for book in user_need_books: 
         user_need_books_name.append(book.name)
+    query = functools.reduce(operator.and_, (Q(name__contains = i) for i in user_need_books_name))    
     users_have_book = BookHave.objects.filter(
-            Q(name__in=user_need_books_name) | ~Q(user=request.user)
+            query
         )
     context['book_have_list'] = users_have_book
     
-    
+    #Books I have and others need
     user_have_books = BookHave.objects.filter(user=request.user)
     user_have_books_name = list()
     for book in user_have_books: 
         user_have_books_name.append(book.name)
+    query2 = functools.reduce(operator.and_, (Q(name__contains = i) for i in user_have_books_name))
     users_need_book = BookNeed.objects.filter(
-            Q(name__in=user_have_books_name) | ~Q(user=request.user)
+            query2
         )
     context['book_need_list'] = users_need_book
     
+    # Logic returns QuerySet (I have and others need) & (I need others have) == True
     
     users_have_book_name = list()
     for book in users_have_book:
         users_have_book_name.append(book.user)
-    
     book_have_match_list = users_need_book.filter(user__in=users_have_book_name)
+    
     
     users_need_book_name = list()
     for book in users_need_book:
         users_need_book_name.append(book.user)
-    
     book_need_match_list = users_have_book.filter(user__in=users_need_book_name)
+    
     
     # Book I have (someone need it)
     context['book_have_match_list'] = book_have_match_list
